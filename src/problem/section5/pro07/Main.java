d29b81a3153571b1a35a05392e1426790848ccea
kaaang <kaaang@kakao.com>
2022-07-01 02:20:22 +0900

    03

--- src/problem/section5/pro07/Main.java
+++ src/problem/section5/pro07/Main.java
@@ -5,14 +5,30 @@ import java.util.*;
 public class Main {
 
 
-    public void solution(){
+    public String solution(String str1, String str2){
+        String answer = "YES";
+        Queue<Character> q = new LinkedList<>();
+        for(char x : str1.toCharArray()) q.offer(x);
+        for(char x : str2.toCharArray()){
+            if(q.contains(x)){
+                if(x != q.poll()){
+                    return "NO";
+                }
+            }
+        }
+        if(!q.isEmpty()) return "NO";
 
+        return answer;
     }
 
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
+
+        String str1 = kb.next();
+        String str2 = kb.next();
+        System.out.println(T.solution(str1, str2));
     }
 
 
